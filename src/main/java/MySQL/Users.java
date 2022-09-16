510f89e64b88014f506fa106a2d1f64aa91b399a
kyleyannelli <kyleyannelli@gmail.com>
2022-09-15 20:56:21 -0400

    updated users table

--- src/main/java/MySQL/Users.java
+++ src/main/java/MySQL/Users.java
@@ -7,8 +7,9 @@ public class Users {
     private long discordId;
     private long songId;
     private long timeSpent;
+    private long serverDiscordId;
 
-    public Users(long discordId, long songId, long timeSpent) {
+    public Users(long discordId, long serverDiscordId, long songId, long timeSpent) {
         this.discordId = discordId;
         this.songId = songId;
         this.timeSpent = timeSpent;
@@ -28,11 +29,12 @@ public class Users {
 
         // save song
         try {
-            String sql = "INSERT INTO users (discord_id, song_id, time_spent) VALUES (?, ?, ?)";
+            String sql = "INSERT INTO users (discord_id, server_discord_id, song_id, time_spent) VALUES (?, ?, ?, ?)";
             PreparedStatement stmt = conn.prepareStatement(sql);
             stmt.setLong(1, discordId);
-            stmt.setLong(2, songId);
-            stmt.setLong(3, timeSpent);
+            stmt.setLong(2, serverDiscordId);
+            stmt.setLong(3, songId);
+            stmt.setLong(4, timeSpent);
             stmt.executeUpdate();
         }
         catch (Exception e) {
@@ -68,6 +70,7 @@ public class Users {
         try {
             String sql = "CREATE TABLE IF NOT EXISTS users (\n"
                     + "    discord_id BIGINT NOT NULL,\n"
+                    + "    server_discord_id BIGINT NOT NULL,\n"
                     + "    song_id BIGINT NOT NULL,\n"
                     + "    time_spent BIGINT NULL,\n"
                     + "    PRIMARY KEY (discord_id, song_id)\n"
