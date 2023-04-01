a6bfc8de6b2e9447cf1ee366fb3ad5ffb5eb5576
Kyle Yannelli <kyleyannelli@gmail.com>
2023-04-01 11:35:11 -0400

    Removed Possible Null Bugs from Users Model

--- src/main/java/MySQL/Users.java
+++ src/main/java/MySQL/Users.java
@@ -3,6 +3,7 @@ package MySQL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Users {
     private long discordId;
@@ -28,31 +29,17 @@ public class Users {
             e.printStackTrace();
             return -1;
         }
-        PreparedStatement stmt;
         // save song
         try {
             String sql = "INSERT INTO users (discord_id, server_discord_id, song_id, time_spent) VALUES (?, ?, ?, ?)";
-            stmt = conn.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
-            stmt.setLong(1, discordId);
-            stmt.setLong(2, serverDiscordId);
-            stmt.setLong(3, songId);
-            stmt.setLong(4, timeSpent);
-            stmt.executeUpdate();
+            assert conn != null;
+            assertAndExecute(songId, serverDiscordId, timeSpent, discordId, conn, sql);
         }
         catch (Exception e) {
             // close connection
             try {
                 // get user id where server_discord_id = serverDiscordId and discord_id = discordId and song_id = songId
-                String sql = "SELECT discord_id FROM users WHERE discord_id = ? AND server_discord_id = ? AND song_id = ?";
-                stmt = conn.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
-                stmt.setLong(1, discordId);
-                stmt.setLong(2, serverDiscordId);
-                stmt.setLong(3, songId);
-                ResultSet rs = stmt.executeQuery();
-                rs.next();
-                long id = rs.getLong(1);
-                conn.close();
-                return id;
+                return executeWhere(conn);
             }
             catch (Exception e2) {
                 System.out.println("Error closing connection");
@@ -65,22 +52,27 @@ public class Users {
         // close connection
         try {
             // get user id where server_discord_id = serverDiscordId and discord_id = discordId and song_id = songId
-            String sql = "SELECT discord_id FROM users WHERE discord_id = ? AND server_discord_id = ? AND song_id = ?";
-            stmt = conn.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
-            stmt.setLong(1, discordId);
-            stmt.setLong(2, serverDiscordId);
-            stmt.setLong(3, songId);
-            ResultSet rs = stmt.executeQuery();
-            rs.next();
-            long id = rs.getLong(1);
-            conn.close();
-            return id;
+            return executeWhere(conn);
         }
         catch (Exception e) {
             System.out.println("Error closing connection");
             e.printStackTrace();
             return -1;
         }
+    }
+
+    private long executeWhere(Connection conn) throws SQLException {
+        PreparedStatement stmt;
+        String sql = "SELECT discord_id FROM users WHERE discord_id = ? AND server_discord_id = ? AND song_id = ?";
+        stmt = conn.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+        stmt.setLong(1, discordId);
+        stmt.setLong(2, serverDiscordId);
+        stmt.setLong(3, songId);
+        ResultSet rs = stmt.executeQuery();
+        rs.next();
+        long id = rs.getLong(1);
+        conn.close();
+        return id;
     }
 
     public static void setupTable(String dB) {
@@ -104,6 +96,7 @@ public class Users {
                     + "    time_spent BIGINT NULL,\n"
                     + "    PRIMARY KEY (discord_id, server_discord_id, song_id)\n"
                     + ")";
+            assert conn != null;
             PreparedStatement stmt = conn.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
             stmt.executeUpdate();
         }
@@ -134,16 +127,11 @@ public class Users {
             e.printStackTrace();
             return;
         }
-        PreparedStatement stmt;
         // save song
         try {
             String sql = "UPDATE users SET time_spent = time_spent + ? WHERE discord_id = ? AND server_discord_id = ? AND song_id = ?";
-            stmt = conn.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
-            stmt.setLong(1, timeSpent);
-            stmt.setLong(2, discordId);
-            stmt.setLong(3, dB);
-            stmt.setLong(4, songId);
-            stmt.executeUpdate();
+            assert conn != null;
+            assertAndExecute(dB, discordId, songId, timeSpent, conn, sql);
         }
         catch (Exception e) {
             System.out.println("Error saving song");
@@ -159,5 +147,16 @@ public class Users {
             System.out.println("Error closing connection");
             e.printStackTrace();
         }
+    }
+
+    private static void assertAndExecute(long dB, long discordId, long songId, long timeSpent, Connection conn, String sql) throws SQLException {
+        PreparedStatement stmt;
+        assert conn != null;
+        stmt = conn.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+        stmt.setLong(1, timeSpent);
+        stmt.setLong(2, discordId);
+        stmt.setLong(3, dB);
+        stmt.setLong(4, songId);
+        stmt.executeUpdate();
     }
 }
