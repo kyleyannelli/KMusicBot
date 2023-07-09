a81cdbb1a4672c760c58fd4458630be8d08d0efe
Kyle Yannelli <kyleyannelli@gmail.com>
2023-07-09 00:39:53 -0400

    UNFINISHED! Started making the Recommender Session & Processor. Original code for testing the recommender / auto queue is still in LavaplayerAudioSource.java. Schedulers, searched songs and POSSIBLY players will move to RecommenderSession. This will also take out some of the complexity from LavaplayerAudioSource.java

---
+++ src/main/java/SongRecommender/RecommenderProcessor.java
@@ -0,0 +1,77 @@
+package SongRecommender;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import org.javacord.api.DiscordApi;
+
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+
+import SpotifyApi.RadioSongsHandler;
+import se.michaelthelin.spotify.SpotifyApi;
+
+public class RecommenderProcessor {
+    private final ExecutorService executorService;
+
+    // Spotify and Discord API objects are persistent the entire application life time
+    private final DiscordApi discordApi;
+    private final SpotifyApi spotifyApi;
+
+    public RecommenderProcessor(DiscordApi discordApi, SpotifyApi spotifyApi, int maxThreads) {
+        this.executorService = Executors.newFixedThreadPool(maxThreads);
+
+        this.discordApi = discordApi;
+        this.spotifyApi = spotifyApi;
+    }
+
+    public void shutdown() {
+        executorService.shutdown();
+        try {
+            if(!executorService.awaitTermination(60, TimeUnit.SECONDS)) {
+                executorService.shutdownNow();
+            }
+        }
+        catch(InterruptedException interruptedException) {
+            executorService.shutdownNow();
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public ArrayList<String> determineSongsFromYoutube(RecommenderSession session) {
+        ArrayList<String> trackNames = new ArrayList<>();
+
+        for(Object song : session.getSearchedSongs()) {
+            String songStr = song.toString();
+            if(songStr.startsWith("https://www.youtube") || songStr.startsWith("https://youtu.be") || songStr.startsWith("https://youtube")) {
+                // If it's a YouTube URL, check if it's already in the queue
+                for(AudioTrack track : session.getAudioQueue()) {
+                    if(track.getInfo().uri.equals(songStr)) {
+                        trackNames.add(track.getInfo().title);
+                        break;
+                    }
+                }
+            } else {
+                // If it's not a YouTube URL, simply add it to trackNames
+                trackNames.add(songStr);
+            }
+        }
+        return trackNames;
+    }
+
+    public String[] getRecommendationsFromSpotify(List<String> songs) { 
+        try {
+            String[] recommendedTracks = RadioSongsHandler.generateRecommendationsFromList(spotifyApi, songs);
+
+            if(recommendedTracks == null || recommendedTracks.length == 0) return new String[0];
+
+            return recommendedTracks;
+        }
+        catch(Exception e) {
+            // log reason
+            return new String[0];
+        }
+    }
+}
---
+++ src/main/java/SongRecommender/RecommenderSession.java
@@ -0,0 +1,63 @@
+package SongRecommender;
+
+import org.javacord.api.DiscordApi;
+import org.javacord.api.audio.AudioConnection;
+import org.javacord.api.event.interaction.SlashCommandCreateEvent;
+
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+
+import se.michaelthelin.spotify.SpotifyApi;
+
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class RecommenderSession {
+	private final int MINIMUM_AUTO_QUEUE_DURATION_SECONDS = 900; // 900 seconds == 15 minutes
+	private final int MINIMUM_QUEUE_SIZE = 4; // at the least 4 songs must be queued
+	private final int MAXIMUM_QUEUE_SIZE = 15; // at the most 15 songs are queued
+
+	// there should only be one recommender processor per application.
+	// 	the processor handles all sessions via an executor service
+	private final RecommenderProcessor recommenderProcessor;
+
+    // AudioConnection should be persistent the entire time the bot is connected to voice channel
+    //  Connect is new session, disconnect should "kill" session
+    private final AudioConnection audioConnection;
+
+	private ArrayList<AudioTrack> audioQueue; // audio queue for the voice channel session
+
+	public RecommenderSession(AudioConnection audioConnection, RecommenderProcessor recommenderProcessor) {
+		this.recommenderProcessor = recommenderProcessor;
+        this.audioConnection = audioConnection;
+	}
+
+
+	public ArrayList<String> getSearchedSongs() {
+		return new ArrayList<String>();
+	}
+
+	public ArrayList<AudioTrack> getAudioQueue() {
+		return new ArrayList<AudioTrack>();
+	}
+
+    private void loadRecommendedTracks(DiscordApi api, SpotifyApi spotifyApi, AudioPlayerManager playerManager, AudioConnection audioConnection, SlashCommandCreateEvent event) {
+        if(canQueueSongs()) {
+            // Process the songs from searched songs list
+            ArrayList<String> trackNames = recommenderProcessor.determineSongsFromYoutube(this);
+			String[] recommendationsFromSpotify = recommenderProcessor.getRecommendationsFromSpotify(trackNames);
+        }
+    }
+
+	private boolean canQueueSongs() {
+        // Calculate the total duration of all songs in the queue
+		AtomicLong totalDuration = new AtomicLong();
+		// for each AudioTrack (song) in the queue add their duration
+		audioQueue.forEach(song -> totalDuration.addAndGet(song.getDuration()));
+
+		// the total duration of tracks is over 15 MINIMUM_AUTO_QUEUE_DURATION_SECONDSu
+		// 	OR the queue is 4 through 15 songs long
+		return totalDuration.get() > MINIMUM_AUTO_QUEUE_DURATION_SECONDS 
+			|| (audioQueue.size() >= MINIMUM_QUEUE_SIZE && audioQueue.size() <= MAXIMUM_QUEUE_SIZE);
+	}
+}
