5941d9dd69e23361d6517c8b253177988227439e
Kyle Yannelli <kyleyannelli@gmail.com>
2023-10-19 23:11:42 -0400

    fix: Moved command handling to executor service. Inherently added MAX_COMMAND_THREADS in .env

--- src/main/java/dev/kmfg/discordbot/listenerhandlers/SlashCommandListenerHandler.java
+++ src/main/java/dev/kmfg/discordbot/listenerhandlers/SlashCommandListenerHandler.java
@@ -3,16 +3,29 @@ package dev.kmfg.discordbot.listenerhandlers;
 import dev.kmfg.discordbot.commands.executors.Command;
 import dev.kmfg.discordbot.commands.intermediates.CommandsRegistry;
 import dev.kmfg.sessions.SessionManager;
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import org.javacord.api.event.interaction.SlashCommandCreateEvent;
 import org.javacord.api.listener.interaction.SlashCommandCreateListener;
+import org.tinylog.Logger;
 
 public class SlashCommandListenerHandler implements SlashCommandCreateListener {
+	// by default allow 10 threads for the pool
+	private final static int DEFAULT_MAX_COMMAND_THREADS = 10;
 	private final SessionManager sessionManager;
 	private final CommandsRegistry commandsRegistry;
+	private final ExecutorService commandExecutorService;
 
 	public SlashCommandListenerHandler(SessionManager sessionManager) {
 		this.sessionManager = sessionManager;
 		this.commandsRegistry = new CommandsRegistry();
+
+		Dotenv.load().get("MAX_COMMAND_THREADS");
+
+		this.commandExecutorService = createCommandExecutorService();
 	}
 
 	@Override
@@ -22,7 +35,22 @@ public class SlashCommandListenerHandler implements SlashCommandCreateListener {
 
 		Command command = this.commandsRegistry.getCommand(slashCommandName, sessionManager, event);
 		if(command != null) {
-			command.execute();
+			commandExecutorService.submit((() -> { command.execute(); }));
+		}
+	}
+
+	protected ExecutorService createCommandExecutorService() {
+		try {
+			return Dotenv.load().get("MAX_COMMAND_THREADS") == null ?
+				Executors.newFixedThreadPool(DEFAULT_MAX_COMMAND_THREADS) :
+				Executors.newFixedThreadPool(
+						Integer.parseInt(Dotenv.load().get("MAX_COMMAND_THREADS"))
+						);
+		}
+		catch (NumberFormatException numberFormatException) {
+			Logger.error("Attempted to parse the number of threads for MAX_COMMAND_THREADS, but got an error instead...\n\tCheck it is an integer in your .env");
+			System.exit(1);
+			throw numberFormatException;
 		}
 	}
 }
