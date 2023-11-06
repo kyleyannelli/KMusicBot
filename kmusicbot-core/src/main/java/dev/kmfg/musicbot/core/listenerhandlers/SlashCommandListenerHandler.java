16e40490b812187778ca12c5dedc54dc9829c280
Harrison Hoggard <harrisonhoggard@gmail.com>
2023-11-06 17:11:14 -0600

    Fixed interaction reply

--- kmusicbot-core/src/main/java/dev/kmfg/musicbot/core/listenerhandlers/SlashCommandListenerHandler.java
+++ kmusicbot-core/src/main/java/dev/kmfg/musicbot/core/listenerhandlers/SlashCommandListenerHandler.java
@@ -66,10 +66,11 @@ public class SlashCommandListenerHandler implements SlashCommandCreateListener {
 		if (user.getRoles(event.getInteraction().getServer().get()).contains(djRole))
 			return true;
 
-		event.getInteraction().createImmediateResponder().respond().thenAccept(action -> action.addEmbed(new EmbedBuilder()
-					.setColor(Color.red)
+		event.getInteraction().createImmediateResponder().respond().thenAccept(action -> {
+			action.addEmbed(new EmbedBuilder()
 					.addField("Permission denied", "You must have the " + djRole.getMentionTag() + " role I created", false))
-				.update());
+					.update();
+		});
 
 		return false;
 	}
