91ee299827b6e9d775081c57ffed01b10bc47fa8
Kyle Yannelli <kyleyannelli@gmail.com>
2024-01-09 04:22:51 +0000

    remove duplicate code

--- kmusicbot-api/src/main/java/dev/kmfg/musicbot/api/controllers/TrackedSongController.java
+++ kmusicbot-api/src/main/java/dev/kmfg/musicbot/api/controllers/TrackedSongController.java
@@ -3,10 +3,7 @@ package dev.kmfg.musicbot.api.controllers;
 import java.io.IOException;
 import java.util.Optional;
 
-import balbucio.discordoauth.DiscordAPI;
-import balbucio.discordoauth.model.User;
 import dev.kmfg.musicbot.api.helpers.GenericHelpers;
-import dev.kmfg.musicbot.api.helpers.KMTokens;
 import dev.kmfg.musicbot.api.routes.ApiV1;
 import dev.kmfg.musicbot.database.models.TrackedSong;
 import spark.Request;
@@ -14,19 +11,9 @@ import spark.Response;
 
 public class TrackedSongController {
     public static String get(Request req, Response res) throws IOException {
-        KMTokens kmTokens = (KMTokens) req.attribute("km-tokens");
-        DiscordAPI discordAPI = new DiscordAPI(kmTokens.getAccessToken());
-        User user = discordAPI.fetchUser();
-
         Optional<TrackedSong> trackedSong = ApiV1.getTrackedSongRepo().findById(Integer.valueOf(req.params(":trackedSongId")));
 
-        if(trackedSong.isEmpty()) {
-            res.type("text");
-            res.status(404);
-            return "Not Found.";
-        }
-
-        if(!GenericHelpers.isUserInGuild(req, trackedSong.get().getGuild().getDiscordId())) {
+        if(trackedSong.isEmpty() || !GenericHelpers.isUserInGuild(req, trackedSong.get().getGuild().getDiscordId())) {
             res.type("text");
             res.status(404);
             return "Not Found.";
